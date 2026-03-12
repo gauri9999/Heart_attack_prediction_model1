95430e313f6bfe28f12b316913695046f29ded0d
Gauri Mahajan <gaurimahajan815@gmail.com>
2026-03-13 00:30:00 +0530

    Update heart_attack__deployment.py

--- heart_attack__deployment.py
+++ heart_attack__deployment.py
@@ -39,9 +39,12 @@ if st.button("Predict"):
         "Troponin":[Troponin]
     })
 
-   prediction = model.predict(df)
+    prediction = model.predict(df)
 
     if prediction[0] == 1:
+        st.error("High Risk of Heart Attack")
+    else:
+        st.success("Low Risk of Heart Attack")
         st.error("High Risk of Heart Attack")
     else:
         st.success("Low Risk of Heart Attack"
